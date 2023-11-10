2c1230966270ad0d42de0fcee27a54e88e34da1a
Juan Vargas <juan_vargas_r@yahoo.com>
2023-11-09 19:52:45 -0500

    semana07, dia01, javascript-dom-key-detect

---
+++ semana07/dia02-javascript-dom-todo-intermedio/main.js
@@ -0,0 +1,17 @@
+// console.log('Hola JS')
+
+const tasks = [] // Aquí almacenaremos las tareasr
+
+const taskInput = document.querySelector('.task__input') // Es la representación de un input desde javascript
+const taskList = document.querySelector('.task__list')
+
+// console.log(taskInput, taskList)
+// console.log({ taskInput, taskList })
+
+taskInput.addEventListener('keypress', function(event) {
+    // Lógica de la aplicación
+    console.log(event.key)
+    if (event.key === 'Enter') {
+      console.log('Agregando una nueva tarea...')
+    }
+  })
