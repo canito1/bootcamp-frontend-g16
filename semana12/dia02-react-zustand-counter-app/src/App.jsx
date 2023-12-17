449798c2daa90813bffe84144df9a1abb17a13f2
Juan Vargas <juan_vargas_r@yahoo.com>
2023-12-16 19:56:04 -0500

    semana12

--- semana12/dia02-react-zustand-counter-app/src/App.jsx
+++ semana12/dia02-react-zustand-counter-app/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCounterStore } from "./store/counter"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { count, increment, decrement } = useCounterStore()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h3>Counter with Zustand</h3>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1rem'}}>
+        <button onClick={increment}>+1</button>
+        <h1>{count}</h1>
+        <button onClick={decrement}>-1</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
